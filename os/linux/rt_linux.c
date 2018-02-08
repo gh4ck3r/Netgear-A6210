654ce59d1babed82f41a7f380a125ef7421d8541
kaduke <kyle@archpc.localdomain>
2018-02-08 13:11:31 -0500

    Remove occurrences of init_timer when using kernel version 4.15+ since the interface was removed

--- os/linux/rt_linux.c
+++ os/linux/rt_linux.c
@@ -104,9 +104,13 @@ static inline void __RTMP_OS_Init_Timer(void *pReserved,
 	PVOID data)
 {
 	if (!timer_pending(pTimer)) {
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 15, 0)
+		timer_setup(pTimer, function, 0);
+#else
 		init_timer(pTimer);
 		pTimer->data = (unsigned long)data;
 		pTimer->function = function;
+#endif
 	}
 }
 
